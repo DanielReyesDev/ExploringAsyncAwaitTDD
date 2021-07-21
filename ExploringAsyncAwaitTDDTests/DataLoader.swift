f411d42333d5522a425e4e9af189bd3a9541080b
Daniel Reyes Sanchez <Daniel.Reyes0@walmart.com>
2021-07-21 15:10:42 -0500

    Delete skeleton file and add utils

---
+++ ExploringAsyncAwaitTDDTests/DataLoader.swift
@@ -0,0 +1,28 @@
+//
+//  DataLoader.swift
+//  ExploringAsyncAwaitTDDTests
+//
+//  Created by Daniel Reyes Sanchez on 21/07/21.
+//
+
+import Foundation
+
+struct DataLoader {
+    
+    private let bundle: Bundle
+    
+    init(identifier: String = "com.walmart.AsyncAwaitExample") {
+        self.bundle = Bundle(identifier: identifier)!
+    }
+    
+    func loadData(from fileName: String) -> Data {
+        guard
+            let url = bundle.url(forResource: fileName, withExtension: "json"),
+            let data = try? Data(contentsOf: url)
+            else {
+                return Data()
+        }
+        return data
+    }
+}
+
